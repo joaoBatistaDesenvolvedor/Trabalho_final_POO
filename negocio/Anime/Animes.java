50e4551076482b60f4188a032c4e845f3120e980
guilherme.092011 <guilherme.092011@hotmail.com>
2021-06-10 15:00:01 -0300

    initial commit

---
+++ negocio/Anime/Animes.java
@@ -0,0 +1,13 @@
+package Anime;
+import Serie.Series;
+
+public class Animes extends Series{
+
+	public Animes(String nome, double duracao, String genero, int ano, String sinopse, int numero_temporadas,
+			int numero_total_epsodios) throws Exception {
+		super(nome, duracao, genero, ano, sinopse, numero_temporadas, numero_total_epsodios);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ negocio/Filme/Filme.java
@@ -0,0 +1,11 @@
+package Filme;
+import Estrutura_videos.Estrutura_videos;
+
+public class Filme extends Estrutura_videos{
+
+	public Filme(String nome, double duracao, String genero, int ano, String sinopse) throws Exception {
+		super(nome, duracao, genero, ano, sinopse);
+		// TODO Auto-generated constructor stub
+	}
+	
+}

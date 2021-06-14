b1da1e1a2932606580e363c44dcd6fcc0b4280e8
guilherme.092011 <guilherme.092011@hotmail.com>
2021-06-14 10:29:17 -0300

    alterações hoje

--- src/apresentacao/TelaAnime.java
+++ src/apresentacao/TelaAnime.java
@@ -2,26 +2,20 @@ package apresentacao;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.JobAttributes;
 
-import javax.swing.JTextField;
-
 import negocio.IFachadaSistema;
+import negocio.Sistema;
 import negocio.Video.anime.Anime;
 
-import java.awt.FlowLayout;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class TelaAnime {
+public class TelaAnime extends JFrame {
 
 	private JFrame frame;
 	private JTextField text_nome_anime;
@@ -30,8 +24,8 @@ public class TelaAnime {
 	private JTextField text_anime_ano_lacamento;
 	private JTextField text_anime_n_Ep;
 	private JTextField text_n_t_temporada;
-	IFachadaSistema IF;
-	Anime anime;
+	private IFachadaSistema IF;
+	private Anime anime;
 
 	/**
 	 * Launch the application.
@@ -60,6 +54,11 @@ public class TelaAnime {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		try {
+			IF = Sistema.getFachada();
+		} catch(Exception e) {
+			JPanelMesssages.erro(this, e.getMessage());
+		}
 		frame = new JFrame();
 		frame.getContentPane().setForeground(Color.WHITE);
 		frame.setBounds(100, 100, 656, 424);
@@ -67,7 +66,7 @@ public class TelaAnime {
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Animes");
-		lblNewLabel.setBounds(47, 5, 387, 113);
+		lblNewLabel.setBounds(47, 5, (int) (frame.getWidth() * 0.8), 113);
 		lblNewLabel.setFont(new Font("Georgia", Font.BOLD, 99));
 		lblNewLabel.setForeground(Color.BLACK);
 		frame.getContentPane().add(lblNewLabel);
@@ -81,7 +80,7 @@ public class TelaAnime {
 		frame.getContentPane().add(text_nome_anime);
 		text_nome_anime.setColumns(10);
 		
-		JLabel lblDuracao = new JLabel("Duração");
+		JLabel lblDuracao = new JLabel("Duracao");
 		lblDuracao.setBounds(239, 125, 59, 15);
 		frame.getContentPane().add(lblDuracao);
 		
@@ -90,7 +89,7 @@ public class TelaAnime {
 		frame.getContentPane().add(text_duracao_anime);
 		text_duracao_anime.setColumns(10);
 		
-		JLabel lblGenero = new JLabel("Gênero");
+		JLabel lblGenero = new JLabel("Genero");
 		lblGenero.setBounds(422, 125, 52, 15);
 		frame.getContentPane().add(lblGenero);
 		
@@ -99,7 +98,7 @@ public class TelaAnime {
 		frame.getContentPane().add(text_anime_genero);
 		text_anime_genero.setColumns(10);
 		
-		JLabel lblAnoDeLanamento = new JLabel("Ano de Lançamento");
+		JLabel lblAnoDeLanamento = new JLabel("Ano de Lancamento");
 		lblAnoDeLanamento.setBounds(136, 149, 140, 15);
 		frame.getContentPane().add(lblAnoDeLanamento);
 		
@@ -114,9 +113,14 @@ public class TelaAnime {
 		
 		JTextArea text_anime_sinopse = new JTextArea();
 		text_anime_sinopse.setBounds(492, 154, 140, 87);
-		frame.getContentPane().add(text_anime_sinopse);
+		text_anime_sinopse.setLineWrap(true);
+		text_anime_sinopse.setWrapStyleWord(true);
+		text_anime_sinopse.setAutoscrolls(true);
+		JScrollPane scrollTextArea = new JScrollPane(text_anime_sinopse);
+		scrollTextArea.setBounds(492, 154, 140, 87);
+		frame.getContentPane().add(scrollTextArea);
 		
-		JLabel lbl_n_t_ep = new JLabel("Número Total de Episódios");
+		JLabel lbl_n_t_ep = new JLabel("Numero Total de Episodios");
 		lbl_n_t_ep.setBounds(6, 176, 135, 15);
 		frame.getContentPane().add(lbl_n_t_ep);
 		
@@ -125,7 +129,7 @@ public class TelaAnime {
 		frame.getContentPane().add(text_anime_n_Ep);
 		text_anime_n_Ep.setColumns(10);
 		
-		JLabel lbl_n_t_ep_1 = new JLabel("Número Total de Temporadas");
+		JLabel lbl_n_t_ep_1 = new JLabel("Numero Total de Temporadas");
 		lbl_n_t_ep_1.setBounds(6, 224, 218, 15);
 		frame.getContentPane().add(lbl_n_t_ep_1);
 		
@@ -137,20 +141,20 @@ public class TelaAnime {
 		JButton btnCadastrarAnime = new JButton("CADASTRAR ANIME");
 		btnCadastrarAnime.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			Anime novo_anime= new Anime();
+			anime = new Anime();
 			try {
-				novo_anime.setNome(text_nome_anime.getText());
-				novo_anime.setDuracao(Double.parseDouble(text_duracao_anime.getText()));
-				novo_anime.setAno(Integer.parseInt(text_anime_ano_lacamento.getText()));
-				novo_anime.setGenero(text_anime_genero.getText());
-				novo_anime.setSinopse(text_anime_sinopse.getText());
-				novo_anime.setNumero_total_ep(Integer.parseInt(text_anime_n_Ep.getText()));
-				novo_anime.setNumero_temporada(Integer.parseInt(text_n_t_temporada.getText()));
-				IF.cadastrarAnime(novo_anime);
+				anime.setNome(text_nome_anime.getText());
+				anime.setDuracao(Double.parseDouble(text_duracao_anime.getText()));
+				anime.setAno(Integer.parseInt(text_anime_ano_lacamento.getText()));
+				anime.setGenero(text_anime_genero.getText());
+				anime.setSinopse(text_anime_sinopse.getText());
+				anime.setNumero_total_ep(Integer.parseInt(text_anime_n_Ep.getText()));
+				anime.setNumero_temporada(Integer.parseInt(text_n_t_temporada.getText()));
+				IF.cadastrarAnime(anime);
 				
-				//Obs criar a funçoes auxiliares sucesso erro aviso
+				//Obs criar a fun?oes auxiliares sucesso erro aviso
 				
-				JOptionPane.showMessageDialog(btnCadastrarAnime, this, "Anime Cadastrado com sucesso",JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(btnCadastrarAnime, "Nome: " + anime.getNome(),"Anime cadastrado com sucesso",JOptionPane.INFORMATION_MESSAGE);
 				text_nome_anime.setText("");
 				text_duracao_anime.setText("");
 				text_anime_ano_lacamento.setText("");
@@ -161,7 +165,7 @@ public class TelaAnime {
 				
 				
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(btnCadastrarAnime, this, "nao possivel cadastrar o anime", 0);
+				JOptionPane.showMessageDialog(btnCadastrarAnime, this, "Nao e possivel cadastrar o anime", 0);
 				e.printStackTrace();
 			}
 			}
